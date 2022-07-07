5426b77b958ffa2e094330f51a25c664dfc0c163
Chenzj <2998739882@qq.com>
2022-07-07 17:22:51 +0800

    UI项目更新

---
+++ OMDb.WinUI3/OMDb.WinUI3/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using OMDb.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+namespace OMDb.WinUI3
+{
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            RatingService.Init();
+        }
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            myButton.Content = "Clicked";
+            var s = RatingService.GetRatings("test");
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var p in s)
+            {
+                stringBuilder.AppendLine(p.ToString());
+            }
+            RateTextBlock.Text = stringBuilder.ToString();
+        }
+    }
+}
