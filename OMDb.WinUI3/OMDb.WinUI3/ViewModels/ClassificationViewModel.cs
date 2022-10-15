859081339bfa6782288714d16355315a37286dd0
QEDSD <2998739882@qq.com>
2022-10-16 00:20:17 +0800

    分类页

---
+++ OMDb.WinUI3/OMDb.WinUI3/ViewModels/ClassificationViewModel.cs
@@ -0,0 +1,53 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Xaml.Media.Imaging;
+using OMDb.WinUI3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace OMDb.WinUI3.ViewModels
+{
+    internal class ClassificationViewModel : ObservableObject
+    {
+        private List<BannerItem> bannerItemsSource;
+        public List<BannerItem> BannerItemSource
+        {
+            get => bannerItemsSource;
+            set => SetProperty(ref bannerItemsSource, value);
+        }
+        public ICommand CheckDetailBannerItemCommand => new RelayCommand<BannerItem>((item) =>
+        {
+
+        });
+        public ClassificationViewModel()
+        {
+            var items = new List<BannerItem>();
+            items.Add(new BannerItem()
+            {
+                Title = "标题1",
+                Description = "描述",
+                Img = new BitmapImage(new Uri(System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Assets/Img/defaultbanner.jpg"))),
+                PreviewImg = new BitmapImage(new Uri(System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Assets/Img/defaultbanneritem.jpg")))
+            });
+            items.Add(new BannerItem()
+            {
+                Title = "标题2",
+                Description = "描述",
+                Img = new BitmapImage(new Uri(System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Assets/Img/defaultbanner.jpg"))),
+                PreviewImg = new BitmapImage(new Uri(System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Assets/Img/defaultbanneritem.jpg")))
+            });
+            items.Add(new BannerItem()
+            {
+                Title = "标题3",
+                Description = "描述",
+                Img = new BitmapImage(new Uri(System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Assets/Img/defaultbanner.jpg"))),
+                PreviewImg = new BitmapImage(new Uri(System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Assets/Img/defaultbanneritem.jpg")))
+            });
+            BannerItemSource = items;
+        }
+    }
+}
---
+++ OMDb.WinUI3/OMDb.WinUI3/Views/ClassificationPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+namespace OMDb.WinUI3.Views
+{
+    public sealed partial class ClassificationPage : Page
+    {
+        public ClassificationPage()
+        {
+            this.InitializeComponent();
+            SizeChanged += ClassificationPage_SizeChanged;
+        }
+
+        private void ClassificationPage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Banner.Height = this.ActualHeight * 0.8;
+        }
+    }
+}
