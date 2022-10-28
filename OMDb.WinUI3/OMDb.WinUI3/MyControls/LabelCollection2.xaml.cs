65ed5e0c65fc3f54c59655ba85227645d8732d30
chenzhaoji <chenzhaoji@genomics.cn>
2022-10-28 17:54:45 +0800

    添加LabelCollection2

---
+++ OMDb.WinUI3/OMDb.WinUI3/MyControls/LabelCollection2.xaml.cs
@@ -0,0 +1,143 @@
+﻿using CommunityToolkit.WinUI.UI.Controls;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using OMDb.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows.Input;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+namespace OMDb.WinUI3.MyControls
+{
+    public sealed partial class LabelCollection2 : UserControl
+    {
+        public LabelCollection2()
+        {
+            this.InitializeComponent();
+        }
+        public static readonly DependencyProperty ItemsSourceProperty = DependencyProperty.Register(
+            "ItemsSource", typeof(IEnumerable<Entry>), typeof(LabelCollection2), new PropertyMetadata(null, new PropertyChangedCallback(OnItemsSourceChanged)));
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(
+           "Title", typeof(string), typeof(LabelCollection2), new PropertyMetadata(null, new PropertyChangedCallback(OnTitleChanged)));
+        public static readonly DependencyProperty DescriptionProperty = DependencyProperty.Register(
+            "Description",typeof(string),typeof(LabelCollection2),new PropertyMetadata(null, new PropertyChangedCallback(OnDescriptionhanged)));
+        public static readonly DependencyProperty DetailCommandProperty= DependencyProperty.Register(
+            nameof(DetailCommand),typeof(ICommand),typeof(LabelCollection2), new PropertyMetadata(default(ICommand)));
+        public static readonly DependencyProperty BgImageSourceProperty = DependencyProperty.Register(
+            "BgImageSource", typeof(ImageSource), typeof(LabelCollection2), new PropertyMetadata(null, new PropertyChangedCallback(OnBgImageSourceChanged)));
+        public static readonly DependencyProperty ClickItemCommandProperty = DependencyProperty.Register(
+            nameof(ClickItemCommand), typeof(ICommand), typeof(LabelCollection2), new PropertyMetadata(default(ICommand)));
+        private static void OnItemsSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            (d as LabelCollection2).ItemsList.ItemsSource = e.NewValue as IEnumerable<Entry>;
+        }
+        private static void OnTitleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            (d as LabelCollection2).TitleTextBlock.Text = e.NewValue as string;
+        }
+        private static void OnDescriptionhanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            (d as LabelCollection2).DesTextBlock.Text = e.NewValue as string;
+        }
+        private static void OnBgImageSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            (d as LabelCollection2).BgImage.ImageSource = e.NewValue as ImageSource;
+        }
+        public IEnumerable<Entry> ItemsSource
+        {
+            get { return (IEnumerable<Entry>)GetValue(ItemsSourceProperty); }
+
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+
+            set { SetValue(TitleProperty, value); }
+        }
+        public string Description
+        {
+            get { return (string)GetValue(DescriptionProperty); }
+
+            set { SetValue(DescriptionProperty, value); }
+        }
+        public ICommand DetailCommand
+        {
+            get => (ICommand)GetValue(DetailCommandProperty);
+            set => SetValue(DetailCommandProperty, value);
+        }
+        public ICommand ClickItemCommand
+        {
+            get => (ICommand)GetValue(ClickItemCommandProperty);
+            set => SetValue(ClickItemCommandProperty, value);
+        }
+        public ImageSource BgImageSource
+        {
+            get => (ImageSource)GetValue(BgImageSourceProperty);
+            set => SetValue(BgImageSourceProperty, value);
+        }
+
+        private void Item_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            var elment = sender as FrameworkElement;
+            var targetVisual = Microsoft.UI.Xaml.Hosting.ElementCompositionPreview.GetElementVisual(sender as UIElement);
+            var animation = targetVisual.Compositor.CreateVector3KeyFrameAnimation();
+            animation.Duration = TimeSpan.FromSeconds(0.3);
+            animation.Target = nameof(targetVisual.Offset);
+            var linear = targetVisual.Compositor.CreateLinearEasingFunction();
+            animation.InsertKeyFrame(1.0f, new Vector3((float)elment.Margin.Left, 0, 0), linear);
+            targetVisual.StartAnimation(nameof(targetVisual.Offset), animation);
+        }
+
+        private void Item_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            var elment = sender as FrameworkElement;
+            var targetVisual = Microsoft.UI.Xaml.Hosting.ElementCompositionPreview.GetElementVisual(sender as UIElement);
+            var animation = targetVisual.Compositor.CreateVector3KeyFrameAnimation();
+            animation.Duration = TimeSpan.FromSeconds(0.3);
+            animation.Target = nameof(targetVisual.Offset);
+            var linear = targetVisual.Compositor.CreateLinearEasingFunction();
+            animation.InsertKeyFrame(1.0f, new Vector3((float)elment.Margin.Left, (float)elment.Margin.Top, 0), linear);
+            targetVisual.StartAnimation(nameof(targetVisual.Offset), animation);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DetailCommand?.Execute(null);
+        }
+
+        private void ItemsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var ad = sender as AdaptiveGridView;
+            if(ad != null && ad.SelectedItem != null)
+            {
+                ClickItemCommand?.Execute(ad.SelectedItem);
+                ad.SelectedItem = null;
+            }
+        }
+
+        private void ItemsList_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            //if((sender as AdaptiveGridView).ActualHeight<this.ActualHeight / 2)
+            //{
+            //    BgImage.Visibility = Visibility.Collapsed;
+            //    BgImage.Height = this.ActualHeight;
+            //    BgImage.Visibility = Visibility.Visible;
+            //}
+            //else
+            //{
+            //    BgImage.Height = this.MaxHeight;
+            //}
+        }
+    }
+}
